18a0c65279c5612ee30084a865c4b10cf56db07d
jaden <18636621167@163.com>
2018-12-08 11:19:13 +0800

    eureka

---
+++ src/main/java/qy/springboot/eureka/EurekaApplication.java
@@ -0,0 +1,15 @@
+package qy.springboot.eureka;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @Author: Jaden
+ * @Description:
+ * @Date: Created in 10:12 2018/12/8
+ * @Modified By:
+ */
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
