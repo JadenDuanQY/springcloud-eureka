aa73c2e2332673dac62e5e7d0d7ea8c68973894c
jaden <18636621167@163.com>
2018-12-08 12:17:58 +0800

    yaml change

--- src/main/java/qy/springboot/eureka/EurekaApplication.java
+++ src/main/java/qy/springboot/eureka/EurekaApplication.java
@@ -1,6 +1,8 @@
 package qy.springboot.eureka;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @Author: Jaden
@@ -8,6 +10,8 @@ import org.springframework.boot.SpringApplication;
  * @Date: Created in 10:12 2018/12/8
  * @Modified By:
  */
+@SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
